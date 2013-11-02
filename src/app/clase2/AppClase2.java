def2d78ef863a896345c4597cb094a95f90d9311
alumno <alumno@LAB706-24>
2013-11-02 16:15:00 -0500

    guardado en local, proyecto Base

---
+++ src/app/clase2/AppClase2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.clase2;
+
+/** shit f6 para compilar
+ *
+/* psvm(tab) para la linea de arriba*
+    * /
+ * @author alumno
+ */
+public class AppClase2 {
+public static void main(String[] args) {
+    System.out.println("hola");
+    
+    
+}
+
+}
